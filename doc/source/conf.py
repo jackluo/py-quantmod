f3d1bc7d68fb81b830a99fd27369fe41bb8e95ef
Jack Luo <thejackluo@gmail.com>
2017-04-24 00:06:23 -0400

    DOC: Update custom CSS

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,9 +20,9 @@
 import os
 import sys
 sys.path.insert(0, "/users/admin/github/py-quantmod")
-#sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath('..'))
-#sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- General configuration ------------------------------------------------
@@ -93,6 +93,9 @@ todo_include_todos = False
 # a list of builtin themes.
 
 html_theme = 'nature'
+
+def setup(app):
+    app.add_stylesheet('custom.css')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
