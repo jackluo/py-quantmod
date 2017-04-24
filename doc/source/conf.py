b8e402110cdcb72e9905d086b233369a4d45cc04
Jack Luo <thejackluo@gmail.com>
2017-04-24 00:16:39 -0400

    DOC: Abspath

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,10 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, "/users/admin/github/py-quantmod")
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- General configuration ------------------------------------------------
