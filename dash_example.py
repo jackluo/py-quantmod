4deda8e48bca0a7166bd2f86fb86471cbb937573
chriddyp <chris@plot.ly>
2017-05-02 16:34:29 -0700

    Show and hide RSI and EMA controls

--- dash_example.py
+++ dash_example.py
@@ -23,38 +23,76 @@ app.css.append_css({
 app.layout = html.Div(
     [
         html.H1('Quantmod Demo | 5-minute App'),
-        core.Dropdown(
-            id='dropdown',
-            options=[
-                dict(label='PowerShares QQQ Trust Series 1', value='QQQ'),
-                dict(label='SPDR S&P 500 ETF Trust', value='SPY'),
-                dict(label='Apple Inc', value='AAPL'),
-                dict(label='Goldman Sachs Group Inc', value='GS'),
-            ],
-            value='SPY',
-        ),
-        core.Dropdown(
-            id='multi',
-            options=[
-                dict(label='EMA', value='EMA'),
-                dict(label='RSI', value='RSI'),
-                dict(label='MACD', value='MACD'),
-                dict(label='BBANDS', value='BBANDS'),
-            ],
-            multi=True,
-            value=[],
-        ),
-        core.Graph(id='output')
-    ]
+        html.Div([
+            html.Span(
+                core.Dropdown(
+                    id='dropdown',
+                    options=[
+                        dict(label='PowerShares QQQ Trust Series 1', value='QQQ'),
+                        dict(label='SPDR S&P 500 ETF Trust', value='SPY'),
+                        dict(label='Apple Inc', value='AAPL'),
+                        dict(label='Goldman Sachs Group Inc', value='GS'),
+                    ],
+                    value='SPY',
+                ), style={'width': '400px', 'display': 'inline-block'}
+            ),
+            html.Span(
+                core.Dropdown(
+                    id='multi',
+                    options=[
+                        dict(label='EMA', value='EMA'),
+                        dict(label='RSI', value='RSI'),
+                        dict(label='MACD', value='MACD'),
+                        dict(label='BBANDS', value='BBANDS'),
+                    ],
+                    multi=True,
+                    value=['EMA']
+                ), style={'width': '400px', 'display': 'inline-block'}
+            )
+        ]),
+        html.Div([
+            html.Div([
+                html.Label('EMA Variable'),
+                core.Input(id='ema-slider')
+            ], id='ema-controls', style={'display': 'none'}),
+            html.Div([
+                html.Label('RSA Variable'),
+                core.Input(id='rsa-slider')
+            ], id='rsa-controls', style={'display': 'none'})
+        ]),
+        core.Graph(id='output', style={'width': '100%'})
+    ], style={'width': '90%', 'margin-left': 'auto', 'margin-right': 'auto'}
 )
 
 
-# In[]:
-# Setup callbacks
+@app.callback(Output('ema-controls', 'style'), [Input('multi', 'value')])
+def display_ema_control(selected_values):
+    if selected_values is None:
+        return {'display': 'none'}
+    elif 'EMA' in selected_values:
+        return {'display': 'block'}
+    else:
+        return {'display': 'none'}
+
+
+
+@app.callback(Output('rsa-controls', 'style'), [Input('multi', 'value')])
+def display_ema_control(selected_values):
+    if selected_values is None:
+        return {'display': 'none'}
+    elif 'RSI' in selected_values:
+        return {'display': 'block'}
+    else:
+        return {'display': 'none'}
+
 
 @app.callback(Output('output', 'figure'), [Input('dropdown', 'value'),
-                                           Input('multi', 'value')])
-def update_graph_from_dropdown(dropdown, multi):
+                                           Input('multi', 'value'),
+                                           Input('rsa-slider', 'value'),
+                                           Input('ema-slider', 'value')])
+def update_graph_from_dropdown(dropdown, multi, rsi_value, ema_value):
+    print(rsi_value)
+    print(ema_value)
     # Get Quantmod Chart
     ch = qm.get_symbol(dropdown, start='2016/01/01')
 
