000d7146c37d42931fba9514ad9e90ec029bd240
Jack Luo <thejackluo@gmail.com>
2017-05-03 12:08:31 -0700

    ENH: Dash update

--- dash_example_full.py
+++ dash_example_full.py
@@ -1,13 +1,12 @@
 # In[]:
 # Import required libraries
 
+import os
 import dash
 import dash_core_components as core
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from flask_caching import Cache
-import os
-
 import quantmod as qm
 
 
